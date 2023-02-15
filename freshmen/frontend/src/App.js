17d6968f2ab248274815e8aba30123f6d63284e8
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-02-15 17:50:56 +0900

    feat : header 컴포넌트 추가

---
+++ freshmen/frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Header";
+
+//Header 컴포넌트 테스트로 넣었습니다!
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
