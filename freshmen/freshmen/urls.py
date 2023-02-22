b2cbabd95c45557661db16baebca6f15af09ec15
jenny <kjn3008@gmail.com>
2023-02-22 23:35:04 +0900

    createsuperuser

--- freshmen/freshmen/urls.py
+++ freshmen/freshmen/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,7 @@ urlpatterns = [
     path('',RedirectView.as_view(url="/main/",permanent=True)),
     path('quiz/', include('quiz.urls')),
     path('guestbook/', include('guestbook.urls')),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'),name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
