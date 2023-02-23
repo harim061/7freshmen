afe1016092c29e62df26dd51a914b94ce602c14e
jenny <kjn3008@gmail.com>
2023-02-24 02:28:06 +0900

    quiz 수정

--- freshmen/quiz/urls.py
+++ freshmen/quiz/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<int:pk>', views.solveName, name='solveName'),
     path('solveQuiz/<int:pk>', views.solveQuiz, name='solveQuiz'),
     path('addQuestion/<int:pk>', views.addQuestion, name='addQuestion'),
+    path('showresult/<int:pk>', views.each_result, name='eachresult'),
+    path('result/<int:pk>',views.result, name='result'),
 ]
