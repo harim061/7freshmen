44bbb2e219eff813b6deedb11b5a8dd0bdceb3da
limsomi <97082128+limsomi@users.noreply.github.com>
2023-02-25 08:24:38 +0900

    Add files via upload

---
+++ freshmen/static/js/Share.js
@@ -0,0 +1,26 @@
+function Share()
+{
+    var share_button=document.getElementById('share');
+    share_button.style.visibility="hidden";
+    share_button.style.display="none";
+    var button_link=document.getElementById('button_link');
+
+    var link=button_link.getElementsByClassName('inputbox');
+    link.item(0).style.display="block";
+    link.item(0).style.visibility="visible";
+    
+    var bubble=document.getElementsByClassName('chatBalloon').item(0);
+    bubble.style.visibility="hidden";
+    // bubble.style.display="none";
+}
+
+function Copy()
+{
+    const val=document.getElementById("link").textContent;
+    const textArea=document.createElement('textarea');
+    document.body.appendChild(textArea);
+    textArea.value=val;
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+}
