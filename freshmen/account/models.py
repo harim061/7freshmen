6083f66662abd23a6f3877e8ac59576ae1893f63
JinHwan <easy_remain@naver.com>
2023-02-25 10:01:39 +0900

    profile

--- freshmen/account/models.py
+++ freshmen/account/models.py
@@ -57,36 +57,6 @@ class User(AbstractUser):
 
 class Profile(models.Model):
 
-    GENDER_CHOICES = {
-        ('male','남성'),
-        ('female','여성'),
-    }
-
-    MBTI_CHOICES = {
-        ('ESTJ','ESTJ'),
-        ('ESTP','ESTP'),
-        ('ESFJ','ESFJ'),
-        ('ESFP','ESFP'),
-        ('ENTJ','ENTJ'),
-        ('ENTP','ENTP'),
-        ('ENFJ','ENFJ'),
-        ('ENFP','ENFP'),
-        ('ISTJ','ISTJ'),
-        ('ISTP','ISTP'),
-        ('ISFJ','ISFJ'),
-        ('ISFP','ISFP'),
-        ('INTJ','INTJ'),
-        ('INTP','INTP'),
-        ('INFJ','INFJ'),
-        ('INFP','INFP'),
-    }
-
-    LIVE_CHOICES ={
-        ('None','None'),
-        ('Y', '유'), 
-        ('N', '무'),
-    }
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     school = models.CharField(max_length=128, null=True, blank=False)
     major = models.CharField(max_length=128, null=True, blank=False)
